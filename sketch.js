2ff6157361f45a88345b10b35995b18c042805c7
harinarayan14 <gjayashankar2008@gmail.com>
2020-04-08 18:00:37 +0530

    first commit of tic tac toe

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function checkForWinner() {
   console.log("checking for winner...")
-  var winner
+  var winner 
   var playerOne = getPlayerOne() //playerOne is always blue!
     //console.log('checkForWinner, playerOne is: ', playerOne)
 var computer = (playerOne === "X") ? "O" : "X"
